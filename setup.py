33091e420c0d62694ea909b6f615e4a1d43329c2
Nick Pawlowski <pawlowski.nick@gmail.com>
2017-06-13 11:56:41 +0100

    updates for pypi packaging, included proper version dependencies and keywords

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='dltk',
       version='0.1',
       description='Deep Learning Toolkit for Medical Image Analysis',
-      author='Martin Rajchl, Nick Pawlowski',
-      #author_email='gward@python.net',
-      #url='https://www.python.org/sigs/distutils-sig/',
-      packages=['dltk'],
-      install_requires=['numpy', 'scipy', 'pandas', 'matplotlib', 'sklearn', 'tensorflow-gpu', 'SimpleITK', 'jupyter']
-     )
+      author='DLTK Contributors',
+      url='https://dltk.github.io',
+      packages=find_packages(exclude=['_docs', 'contrib', 'data', 'examples']),
+      keywords='machine learning tensorflow deep learning biomedical imaging',
+      license='Apache License 2.0',
+      classifiers=['Intended Audience :: Developers',
+                   'Intended Audience :: Education',
+                   'Intended Audience :: Science/Research',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: POSIX :: Linux',
+                   'Operating System :: MacOS :: MacOS X',
+                   'Operating System :: Microsoft :: Windows',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4'],
+      install_requires=['numpy>=1.12.1', 'scipy>=0.19.0', 'pandas>=0.19.0', 'matplotlib>=1.5.3',
+                        'scikit-image>=0.13.0', 'tensorflow-gpu>=1.1.0', 'SimpleITK>=1.0.0', 'jupyter>=1.0.0']
+      )
