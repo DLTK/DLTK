9f429c47ed052452c787ec2c2cc69df556c28954
Martin Rajchl <m.rajchl@imperial.ac.uk>
2017-06-26 18:31:09 +0100

    feat: added smooth dice loss

--- dltk/core/modules/losses.py
+++ dltk/core/modules/losses.py
@@ -59,3 +59,49 @@ def mse(x, y, name='mse', collections=['losses']):
     loss = tf.reduce_mean(tf.square(x - y), name=name)
     scalar_summary(loss, name, collections)
     return loss
+
+
+def dice_loss(logits, labels, num_classes, include_background=False, name='dice_loss', collections=['losses']):
+    """ Smooth dice loss
+
+        Calculates the smooth dice loss and builds a scalar summary.
+
+        Parameters
+        ----------
+        logits : tf.Tensor
+            prediction for which to calculate the error
+        labels : tf.Tensor
+            sparse targets with which to calculate the error
+        num_classes : int 
+            number of class labels to evaluate on
+        include_background : bool
+            flag to include a loss on the background label or not
+        name : string
+            name of this operation and summary
+        collections : list or tuple
+            list of collections to add the summaries to
+
+        Returns
+        -------
+        tf.Tensor
+            Tensor representing the loss
+
+    """
+    
+    probs = tf.nn.softmax(logits)
+    onehot_labels = tf.one_hot(labels, num_classes, dtype=tf.float32, name='onehot_labels')
+    
+    label_sum = tf.reduce_sum(onehot_labels, axis=[1, 2, 3], name='label_sum')
+    pred_sum = tf.reduce_sum(probs, axis=[1, 2, 3], name='pred_sum')
+    
+    intersection = tf.reduce_sum(onehot_labels * probs, axis=[1, 2, 3], name='intersection')
+    per_class_dice = tf.reduce_mean((2. * intersection + 1.) / (label_sum + pred_sum + 1.), axis=0)
+
+    dice = tf.reduce_mean(per_class_dice) if include_background else tf.reduce_mean(per_class_dice[1:])
+    loss = 1. - dice
+    
+    scalar_summary(loss, name, collections)
+
+    return loss
+
+
