0c09ab556dc7345e5e4ba36bd1fd9f4e72e7ca08
Nick Pawlowski <pawlowski.nick@gmail.com>
2017-09-20 17:46:05 +0100

    better error handling

--- dltk/core/io/reader.py
+++ dltk/core/io/reader.py
@@ -5,7 +5,7 @@ from __future__ import division
 import tensorflow as tf
 import numpy as np
 import SimpleITK as sitk
-
+import traceback
 
 class AbstractReader(object):
     """Abstract reader
@@ -175,7 +175,12 @@ class AbstractReader(object):
             list
                 list of things just read
             """
-            ex = self._read_sample(id_queue, **kwargs)
+            try:
+                ex = self._read_sample(id_queue, ** kwargs)
+            except Exception as e:
+                print('got error `{} from `_read_sample`:'.format(e))
+                print(traceback.format_exc())
+                raise
 
             # eventually fix data types of read objects
             tensors = []
