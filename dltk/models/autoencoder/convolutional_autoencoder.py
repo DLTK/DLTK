8d770758d50af0e8832a4bc4de9d8a3dfce23de6
Martin Rajchl <m.rajchl@imperial.ac.uk>
2017-10-27 17:13:17 +0100

    add: conv ae and example training script

---
+++ dltk/models/autoencoder/convolutional_autoencoder.py
@@ -0,0 +1,114 @@
+"""Summary
+"""
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+
+def convolutional_autoencoder_3D(inputs, num_convolutions=1, num_hidden_units=128, filters=(16, 32, 64), 
+                    strides=((2, 2, 2), (2, 2, 2), (2, 2, 2)), 
+                    mode=tf.estimator.ModeKeys.EVAL, use_bias=False, name='conv_autoencoder_3D'):
+    """Convolutional autoencoder with num_convolutions on len(filters) resolution scales. Downsampling features is done via strided convolutions. 
+    
+    Args:
+        inputs (TYPE): Description
+        num_convolutions (int, optional): Description
+        num_hidden_units(int, optional): Description
+        filters (tuple, optional): Description
+        strides (tuple, optional): Description
+        mode (TYPE, optional): Description
+        name (str, optional): Description
+    
+    Returns:
+        TYPE: Description
+    """
+
+    outputs = {}
+    assert len(strides) == len(filters)
+    assert len(inputs.get_shape().as_list()) == 5, 'inputs are required to have a rank of 5.'
+
+    conv_op = tf.layers.conv3d
+    tp_conv_op = tf.layers.conv3d_transpose
+    relu_op = tf.nn.relu6
+    
+    conv_params = {'padding' : 'same',
+                  'use_bias' : use_bias,
+                  'kernel_initializer' : tf.uniform_unit_scaling_initializer(),
+                  'bias_initializer' : tf.zeros_initializer(),
+                  'kernel_regularizer' : None,
+                  'bias_regularizer' : None}
+
+    x = inputs
+    tf.logging.info('Input tensor shape {}'.format(x.get_shape()))
+
+    # Convolutional feature encoding blocks with num_convolutions at different resolution scales res_scales
+    for res_scale in range(0, len(filters)):
+        
+        for i in range(0, num_convolutions - 1):
+            with tf.variable_scope('enc_unit_{}_{}'.format(res_scale, i)):
+                x = conv_op(x, filters[res_scale], (3, 3, 3), (1, 1, 1), **conv_params)
+                x = tf.layers.batch_normalization(x, training=mode==tf.estimator.ModeKeys.TRAIN)
+                x = relu_op(x)       
+                tf.logging.info('Encoder at res_scale {} tensor shape: {}'.format(res_scale, x.get_shape()))
+        
+        # Employ strided convolutions to downsample
+        with tf.variable_scope('enc_unit_{}_{}'.format(res_scale, num_convolutions)):
+            x = conv_op(x, filters[res_scale], (3, 3, 3), strides[res_scale], **conv_params)
+            x = tf.layers.batch_normalization(x, training=mode==tf.estimator.ModeKeys.TRAIN)
+            x = relu_op(x)  
+            tf.logging.info('Encoder at res_scale {} tensor shape: {}'.format(res_scale, x.get_shape()))
+        
+    # Densely connected layer of hidden units 
+    x_shape = x.get_shape().as_list() 
+    x = tf.reshape(x, (tf.shape(x)[0], np.prod(x_shape[1:])))        
+    
+    x = tf.layers.dense(x,
+                        num_hidden_units,
+                        use_bias=conv_params['use_bias'],
+                        kernel_initializer=conv_params['kernel_initializer'],
+                        bias_initializer=conv_params['bias_initializer'],
+                        kernel_regularizer=conv_params['kernel_regularizer'],
+                        bias_regularizer=conv_params['bias_regularizer'],
+                        name='hidden_units')
+
+    outputs['hidden_units'] = x
+    tf.logging.info('Hidden units tensor shape: {}'.format(x.get_shape()))
+    
+    x = tf.layers.dense(x,
+                        np.prod(x_shape[1:]),
+                        use_bias=conv_params['use_bias'],
+                        kernel_initializer=conv_params['kernel_initializer'],
+                        bias_initializer=conv_params['bias_initializer'],
+                        kernel_regularizer=conv_params['kernel_regularizer'],
+                        bias_regularizer=conv_params['bias_regularizer'])
+
+    x = tf.reshape(x, [tf.shape(x)[0]] + list(x_shape)[1:])
+    tf.logging.info('Decoder input tensor shape: {}'.format(x.get_shape()))
+    
+    # Decoding blocks with num_convolutions at different resolution scales res_scales
+    for res_scale in reversed(range(0, len(filters))):
+        
+        # Employ strided transpose convolutions to upsample
+        with tf.variable_scope('dec_unit_{}_0'.format(res_scale)):
+            x = tp_conv_op(x, filters[res_scale], (3, 3, 3), strides[res_scale], **conv_params)
+            x = tf.layers.batch_normalization(x, training=mode==tf.estimator.ModeKeys.TRAIN)
+            x = relu_op(x)
+            tf.logging.info('Decoder at res_scale {} tensor shape: {}'.format(res_scale, x.get_shape()))
+
+        for i in range(1, num_convolutions):
+            with tf.variable_scope('dec_unit_{}_{}'.format(res_scale, i)):
+                x = conv_op(x, filters[res_scale], (3, 3, 3), (1, 1, 1), **conv_params)
+                x = tf.layers.batch_normalization(x, training=mode==tf.estimator.ModeKeys.TRAIN)
+                x = relu_op(x)  
+            tf.logging.info('Decoder at res_scale {} tensor shape: {}'.format(res_scale, x.get_shape())) 
+     
+    # A final convolution reduces the number of output features to those of the inputs
+    x = conv_op(x, inputs.get_shape().as_list()[-1], (1, 1, 1), (1, 1, 1), **conv_params)
+    
+    tf.logging.info('Output tensor shape: {}'.format(x.get_shape()))
+    outputs['x_'] = x
+    
+    return outputs
---
+++ examples/applications/convolutional_autoencoder/train.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+#from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from builtins import input
+
+import argparse
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from dltk.core.metrics import *
+from dltk.core.losses import *
+from dltk.models.autoencoder.convolutional_autoencoder import convolutional_autoencoder_3D
+from dltk.io.abstract_reader import Reader
+from reader import receiver, save_fn
+
+
+# PARAMS
+SAVE_SUMMARY_STEPS = 10
+SAVE_EVERY_N_STEPS = 10
+
+NUM_CLASSES = 9
+
+STEPS_EVAL = 1000
+MAX_STEPS = 100000
+
+
+# MODEL
+def model_fn(features, labels, mode, params):
+    """Summary
+    
+    Args:
+        features (TYPE): Description
+        labels (TYPE): Description
+        mode (TYPE): Description
+        params (TYPE): Description
+    
+    Returns:
+        TYPE: Description
+    """
+
+    # 1. create a model and its outputs
+    net_output_ops = convolutional_autoencoder_3D(features['x'], num_convolutions=2, mode=mode)
+    
+    # 1.1 Generate predictions only (for `ModeKeys.PREDICT`)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=net_output_ops,
+                                          export_outputs={'out': tf.estimator.export.PredictOutput(net_output_ops)})
+    # 2. set up a loss function
+    loss = tf.losses.mean_squared_error(labels=features['x'], predictions=net_output_ops['x_'])
+    
+    # 3. define a training op and ops for updating moving averages (i.e. for batch normalisation)  
+    global_step = tf.train.get_global_step()
+    optimiser = tf.train.AdamOptimizer(learning_rate=params["learning_rate"], epsilon=1e-5)
+      
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        train_op = optimiser.minimize(loss, global_step=global_step)
+    
+    # 4.1 (optional) create custom image summaries for tensorboard
+    my_image_summaries = {}
+    my_image_summaries['feat_t1'] = features['x'][0,0,:,:,0]
+    my_image_summaries['feat_t1_ir'] = features['x'][0,0,:,:,1]
+    my_image_summaries['feat_t2_flair'] = features['x'][0,0,:,:,2]
+    my_image_summaries['pred_t1'] = tf.cast(net_output_ops['x_'], tf.float32)[0,0,:,:,0]
+    my_image_summaries['pred_t1_ir'] = tf.cast(net_output_ops['x_'], tf.float32)[0,0,:,:,1]
+    my_image_summaries['pred_t2_flair'] = tf.cast(net_output_ops['x_'], tf.float32)[0,0,:,:,2]
+        
+    expected_output_size = [1, 240, 240, 1] # [B, W, H, C]
+    [tf.summary.image(name, tf.reshape(image, expected_output_size)) for name, image in my_image_summaries.items()]
+    
+    # 5. Return EstimatorSpec object
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=net_output_ops, loss=loss, train_op=train_op, eval_metric_ops=None)
+
+
+def train(args):
+
+    np.random.seed(42)
+    tf.set_random_seed(42)
+
+    print('Setting up...')
+
+    # Parse csv files for file names
+    train_filenames = pd.read_csv(args.train_csv, dtype=object, keep_default_na=False, na_values=[]).as_matrix()
+    val_filenames = pd.read_csv(args.val_csv, dtype=object, keep_default_na=False, na_values=[]).as_matrix()
+    
+    # Set up a data reader to handle the file i/o. 
+    reader_params = {'n_examples': 18, 'example_size': [8, 240, 240], 'extract_examples': True}
+    reader_example_shapes = {'features': {'x': reader_params['example_size'] + [3,]},
+                             'labels': {'y': reader_params['example_size']}}
+    reader = Reader(receiver, save_fn, {'features': {'x': tf.float32}, 'labels': {'y': tf.int32}})
+
+    # Get input functions and queue initialisation hooks for training and validation data
+    train_input_fn, train_qinit_hook = reader.get_inputs(train_filenames, tf.estimator.ModeKeys.TRAIN,
+                                                         example_shapes=reader_example_shapes, params=reader_params)
+    val_input_fn, val_qinit_hook = reader.get_inputs(val_filenames, tf.estimator.ModeKeys.EVAL,
+                                                     example_shapes=reader_example_shapes, params=reader_params)
+        
+    # Instantiate the neural network estimator
+    nn = tf.estimator.Estimator(model_fn=model_fn, model_dir=args.save_path, params={"learning_rate": 0.001})
+    
+    # Hooks for training and validation summaries
+    train_summary_hook  = tf.contrib.training.SummaryAtEndHook(args.save_path)
+    val_summary_hook  = tf.contrib.training.SummaryAtEndHook(os.path.join(args.save_path, 'eval'))
+    
+    print('Starting training...')
+    try:
+        while True:
+            nn.train(input_fn=train_input_fn, hooks=[train_qinit_hook, train_summary_hook], steps=100)
+
+            if args.run_validation:
+                results_val = nn.evaluate(input_fn=val_input_fn, hooks=[val_qinit_hook, val_summary_hook], steps=10)
+                print('Step = {}; val loss = {:.5f};'.format(results_val['global_step'], results_val['loss']) )
+
+    except KeyboardInterrupt:
+        print('Stopping now.')
+        export_dir = nn.export_savedmodel(export_dir_base=args.save_path,
+            serving_input_receiver_fn=reader.serving_input_receiver_fn(reader_example_shapes))
+        print('Model saved to {}.'.format(export_dir))
+        
+        
+if __name__ == '__main__':
+
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description='Example: generic training script')
+    parser.add_argument('--run_validation', default=True)
+    parser.add_argument('--resume', default=False, action='store_true')
+    parser.add_argument('--verbose', default=False, action='store_true')
+    parser.add_argument('--cuda_devices', '-c', default='0')
+    
+    parser.add_argument('--save_path', '-p', default='/tmp/mrbrains_autoencoder/')
+    parser.add_argument('--train_csv', default='train.csv')
+    parser.add_argument('--val_csv', default='val.csv')
+    
+    args = parser.parse_args()
+        
+    # Set verbosity
+    if args.verbose:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+        tf.logging.set_verbosity(tf.logging.INFO)
+    else:
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+        tf.logging.set_verbosity(tf.logging.ERROR)
+
+    # GPU allocation options
+    os.environ["CUDA_VISIBLE_DEVICES"] = args.cuda_devices
+    
+    # Create model save path
+    #os.system("rm -rf %s" % args.save_path)
+    #os.system("mkdir -p %s" % args.save_path)
+
+    # Call training
+    train(args)
