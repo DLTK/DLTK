dae7117f090c7050378f543cb491bfa398b47342
Martin Rajchl <m.rajchl@imperial.ac.uk>
2017-11-14 20:50:00 +0000

    clean up and add docstrings to readers.

--- examples/applications/IXI_HH_representation_learning_cae/reader.py
+++ examples/applications/IXI_HH_representation_learning_cae/reader.py
@@ -1,38 +1,41 @@
 import SimpleITK as sitk
 import tensorflow as tf
 import os
-import glob
 
 from dltk.io.augmentation import *
 from dltk.io.preprocessing import *
 
+
 def read_fn(file_references, mode, params=None):
-    """Summary
+    """A custom python read function for interfacing with nii image files.
     
     Args:
-        file_references (TYPE): Description
-        mode (TYPE): Description
-        params (TYPE): Description
+        file_references (list): A list of lists containing file references, such as [['id_0', 'image_filename_0', target_value_0], ..., ['id_N', 'image_filename_N', target_value_N]].
+        mode (str): One of the tf.estimator.ModeKeys strings: TRAIN, EVAL or PREDICT.
+        params (dict, optional): A dictionary to parameterise read_fn ouputs (e.g. reader_params = {'n_examples': 10, 'example_size': [64, 64, 64], 'extract_examples': True}, etc.).
     
-    Returns:
-        TYPE: Description
+    Yields:
+        dict: A dictionary of reader outputs for dltk.io.abstract_reader. 
     """
     
-    def _augment(images):
-        images = add_gaussian_noise(images, sigma=0.1)
+    def _augment(img):
+        """An image augmentation function. 
         
-        return images
+        Args:
+            img (np.array): Input image to be augmented. 
+        
+        Returns:
+            np.array: The augmented image.
+        """
+        
+        return add_gaussian_noise(img, sigma=0.1)
 
-    i = 0
-    while True:
-
-        subject_id = file_references[i][0]
-        i += 1
-        if i == len(file_references):
-            i = 0
+    for f in file_references:
+        subject_id = f[0]
             
         data_path = '../../../data/IXI_HH/1mm'
         
+        # Read the image nii with sitk
         t1_fn = os.path.join(data_path, '{}/T1_1mm.nii.gz'.format(subject_id))
         t2_fn = os.path.join(data_path, '{}/T2_1mm.nii.gz'.format(subject_id))
         pd_fn = os.path.join(data_path, '{}/PD_1mm.nii.gz'.format(subject_id))
