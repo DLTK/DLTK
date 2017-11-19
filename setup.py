129cb8eda2ced9245c1b4ab47afc60d250eaf8cf
Nick Pawlowski <pawlowski.nick@gmail.com>
2017-11-19 11:55:48 +0000

    bump version to keep status before v0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='dltk',
-      version='0.1',
+      version='0.1.1',
       description='Deep Learning Toolkit for Medical Image Analysis',
       author='DLTK Contributors',
       url='https://dltk.github.io',
