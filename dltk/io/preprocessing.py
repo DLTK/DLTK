FIX: normalisation maximum computation and checks.

dltk/io/preprocessing.py
@@ -5,13 +5,20 @@ from __future__ import absolute_import
 
 import numpy as np
 
-epsilon = 1e-8
-
 
 def whitening(image):
     """Whitening. Normalises image to zero mean and unit variance."""
 
-    ret = (image - np.mean(image)) / (np.std(image) + epsilon)
+    image = image.astype(np.float32)
+    
+    mean = np.mean(image)
+    std = np.std(image)
+    
+    if std > 0:
+        ret = (image - mean) / std 
+    else: 
+        ret = image * 0.
+        
     return ret
 
 
@@ -19,8 +26,15 @@ def normalise_zero_one(image):
     """Image normalisation. Normalises image to fit [0, 1] range."""
 
     image = image.astype(np.float32)
-    ret = (image - np.min(image))
-    ret /= (np.max(image) + epsilon)
+    
+    minimum = np.min(image)
+    maximum = np.max(image)
+    
+    if maximum > minimum:
+        ret = (image - minimum) / (maximum - minimum)
+    else:
+        ret = image * 0.
+        
     return ret
 
 