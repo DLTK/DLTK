1928e92633aa1a4acd37c774a9239f58bf6d0c65
Nick Pawlowski <pawlowski.nick@gmail.com>
2018-10-24 22:58:10 -0400

    add variational se

--- dltk/core/units/residual_unit.py
+++ dltk/core/units/residual_unit.py
@@ -556,3 +556,142 @@ def se_resnext_unit_3d(inputs,
         x += orig_x
 
     return x
+
+
+def variational_se_resnext_unit_3d(inputs,
+                                   out_filters,
+                                   cardinality=32,
+                                   bottleneck_channels=4,
+                                   reduction_ratio=16,
+                                   kernel_size=(3, 3, 3),
+                                   mode=tf.estimator.ModeKeys.EVAL,
+                                   use_bias=False,
+                                   activation=tf.nn.leaky_relu,
+                                   kernel_initializer=tf.initializers.variance_scaling(distribution='uniform'),
+                                   bias_initializer=tf.zeros_initializer(),
+                                   kernel_regularizer=None,
+                                   bias_regularizer=None):
+    """Implementation of a 3D residual unit according to [1]. This
+        implementation supports strided convolutions and automatically
+        handles different input and output filters.
+
+    [1] S. Xie et al. Aggregated Residual Transformations for Deep Neural
+        Networks. arXiv 2016.
+    [2] J. Hu et al. Squeeze-and-excitation networks. CVPR 2017.
+
+    Args:
+        inputs (tf.Tensor): Input tensor to the residual unit. Is required to
+            have a rank of 5 (i.e. [batch, x, y, z, channels]).
+        out_filters (int): Number of convolutional filters used in
+            the sub units.
+        cardinality (int): Number of convolution groups in the resnext unit.
+        bottleneck_channels (int): Number of channels per group.
+        reduction_ratio (int): Ratio to reduce filters in SE-MLP with.
+        kernel_size (tuple, optional): Size of the convoltional kernels
+            used in the sub units
+        mode (str, optional): One of the tf.estimator.ModeKeys: TRAIN, EVAL or
+            PREDICT
+        activation (optional): A function to use as activation function.
+        use_bias (bool, optional): Train a bias with each convolution.
+        kernel_initializer (TYPE, optional): Initialisation of convolution kernels
+        bias_initializer (TYPE, optional): Initialisation of bias
+        kernel_regularizer (None, optional): Additional regularisation op
+        bias_regularizer (None, optional): Additional regularisation op
+
+    Returns:
+        tf.Tensor: Output of the residual unit
+    """
+
+    conv_params = {'padding': 'same',
+                   'use_bias': use_bias,
+                   'kernel_initializer': kernel_initializer,
+                   'bias_initializer': bias_initializer,
+                   'kernel_regularizer': kernel_regularizer,
+                   'bias_regularizer': bias_regularizer}
+
+    in_filters = inputs.get_shape().as_list()[-1]
+    assert in_filters == inputs.get_shape().as_list()[-1], \
+        'Module was initialised for a different input shape'
+
+    x = inputs
+    orig_x = x
+
+    # if TF group convolutions exist use:
+    # bottleneck_size = bottleneck_channels * cardinality
+
+    with tf.variable_scope('sub_unit_convs'):
+        x = tf.layers.batch_normalization(
+            x, training=mode == tf.estimator.ModeKeys.TRAIN)
+        x = activation(x)
+
+        pre_split = x
+
+        groups = []
+        for group in range(cardinality):
+            with tf.variable_scope('group_{}'.format(group)):
+                x = tf.layers.conv3d(
+                    inputs=pre_split,
+                    filters=bottleneck_channels,
+                    kernel_size=1,
+                    **conv_params)
+
+                x = tf.layers.conv3d(
+                    inputs=x,
+                    filters=bottleneck_channels,
+                    kernel_size=kernel_size,
+                    **conv_params)
+
+                groups.append(x)
+
+        x = tf.concat(groups, axis=-1)
+
+        x = tf.layers.conv3d(
+            inputs=x,
+            filters=out_filters,
+            kernel_size=1,
+            **conv_params)
+
+    with tf.variable_scope('sub_se_unit'):
+        pooled = tf.reduce_mean(x, axis=[1, 2, 3])
+
+        scale = tf.layers.dense(pooled, out_filters // reduction_ratio)
+
+        scale = tf.nn.relu(scale)
+
+        scale_mu = tf.layers.dense(scale, out_filters)
+
+        scale_log_sig_sq = tf.layers.dense(scale, out_filters)
+
+        sampled_scale = (scale_mu + tf.random_normal(tf.shape(scale_mu))
+                         * tf.sqrt(tf.exp(scale_log_sig_sq)))
+
+        kl_div = -0.5 * tf.reduce_mean(
+            tf.reduce_sum(1 + scale_log_sig_sq - tf.square(scale_mu)
+                          - tf.exp(scale_log_sig_sq), 1))
+
+        tf.add_to_collection('variational_se_kl', kl_div)
+
+        scale = tf.reshape(sampled_scale, [-1, 1, 1, 1, out_filters])
+
+        x = scale * x
+
+    # Add the residual
+    with tf.variable_scope('sub_unit_add'):
+        # Handle differences in input and output filter sizes
+        if in_filters < out_filters:
+            orig_x = tf.pad(
+                tensor=orig_x,
+                paddings=[[0, 0]] * (len(x.get_shape().as_list()) - 1) + [[
+                    int(np.floor((out_filters - in_filters) / 2.)),
+                    int(np.ceil((out_filters - in_filters) / 2.))]])
+
+        elif in_filters > out_filters:
+            orig_x = tf.layers.conv3d(
+                inputs=orig_x,
+                filters=out_filters,
+                kernel_size=kernel_size,
+                strides=(1, 1, 1),
+                **conv_params)
+        x += orig_x
+
+    return x
